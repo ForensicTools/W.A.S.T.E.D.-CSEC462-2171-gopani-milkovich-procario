cfcf1ff3a8f7f50a2a132e2aec0faf2e29ac535c
ImTh3Jok3r <mjm1276@mail.rit.edu>
2017-11-13 10:53:54 -0500

    No longer creates local file.

--- pcapimport.py
+++ pcapimport.py
@@ -1,13 +1,13 @@
 import os
 import socket
 import urllib.request
-from contextlib import closing
 import json
+import subprocess
 
 # dictionary to hold all results
 records = {}
 
-def parsingFile(badSites):
+def parsing(badSites, response):
     """
     Open up the results of the tshark on the PCAP.
     Extract the hostnames from the DNS lookups then use them to find IPs
@@ -16,38 +16,36 @@ def parsingFile(badSites):
     :return: Dictionary of all of the hosts visited, the relevant information,
     and the number of times visited.
     """
-    with open("web.txt") as file:
-        for line in file:
-            line = line.split()
-            hostName = line[1]
-            # get the IP using the host name
-            ip = getIP(hostName)[0]
-            # url of the API to grab the location using IP
-            url = 'http://freegeoip.net/json/'
-            # query the API
-            try:
-                with urllib.request.urlopen(url+ip) as response:
-                    location = json.loads(response.read().decode())
-                    # get wanted information from json
-                    lat = location["latitude"]
-                    lon = location["longitude"]
-            except:
-                lat = "unknown"
-                lon = "unknown"
-            if hostName in records.keys():
-                # if host exists, increment counter
-                records[hostName]["NumVisit"] += 1
-            else:
-                # if not, create entry and set visits to 1
-                records[hostName] = {}
-                records[hostName]["NumVisit"] = 1
-                records[hostName]["Latitude"] = lat
-                records[hostName]["Longitude"] = lon
-                records[hostName]["IP"] = ip
-                records[hostName]["KnownBad"] = 0
-                if hostName in badSites:
-                    records[hostName]["KnownBad"]  = 1
-        return records
+    for url in response:
+        hostName = url.strip()
+        # get the IP using the host name
+        ip = getIP(hostName)[0]
+        # url of the API to grab the location using IP
+        locationCheck = 'http://freegeoip.net/json/'
+        # query the API
+        try:
+            with urllib.request.urlopen(locationCheck+ip) as res:
+                location = json.loads(res.read().decode())
+                # get wanted information from json
+                lat = location["latitude"]
+                lon = location["longitude"]
+        except:
+            lat = "unknown"
+            lon = "unknown"
+        if hostName in records.keys():
+            # if host exists, increment counter
+            records[hostName]["NumVisit"] += 1
+        else:
+            # if not, create entry and set visits to 1
+            records[hostName] = {}
+            records[hostName]["NumVisit"] = 1
+            records[hostName]["Latitude"] = lat
+            records[hostName]["Longitude"] = lon
+            records[hostName]["IP"] = ip
+            records[hostName]["KnownBad"] = 0
+            if hostName in badSites:
+                records[hostName]["KnownBad"]  = 1
+    return records
 
 def getIP(hostName):
     """
@@ -59,8 +57,7 @@ def getIP(hostName):
         ip = socket.gethostbyname_ex(hostName.strip())
         return ip[2]
     except:
-        print("Failed: " + hostName)
-        return hostName
+        return "Not Found"
 
 def knownBad(level):
     if level == "high":
@@ -72,15 +69,14 @@ def knownBad(level):
     return badSites
 
 if __name__ == '__main__':
-    # need to fix hardcoded pcap file
-    # need to come up with way to not create file from output and just read command output instead
-
     # Create an inteactive way to choose the PCAP and level of comparison (bad list)
+
     # Low sensitivity has more false positives
     # High has less flase positives
     level = input("Choose level (low,medium,high): ")
     badSites = knownBad(level)
-    os.system('tshark -r web.pcap -T fields -e ip.src -e dns.qry.name -Y "dns.flags.response eq 0" | grep .com > web.txt')
-    results = parsingFile(badSites)
+    response = os.popen('tshark -r web.pcap -T fields -e dns.qry.name -Y "dns.flags.response eq 0" | grep .com').read()
+    response = response.splitlines()
+    results = parsing(badSites,response)
     with open('result.json', 'w') as fp:
             json.dump(results, fp)
