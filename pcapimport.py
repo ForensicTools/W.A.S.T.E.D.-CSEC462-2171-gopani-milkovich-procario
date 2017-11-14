0a30b16fae35b526a2ace8d9c1723b0832824590
ImTh3Jok3r <mjm1276@mail.rit.edu>
2017-11-13 19:45:46 -0500

    Changed functionality to do DNS information. Lots of changes. Ow.

--- pcapimport.py
+++ pcapimport.py
@@ -15,10 +15,13 @@ def parsing(badSites, response):
     :return: Dictionary of all of the hosts visited, the relevant information,
     and the number of times visited.
     """
+    long = []
+    lati = []
     for url in response:
         hostName = url.strip()
-        # get the IP using the host name
+        # get the IP using the host name - used for location
         ip = getIP(hostName)[0]
+        # list of all associated ips
         # url of the API to grab the location using IP
         locationCheck = 'http://freegeoip.net/json/'
         # query the API
@@ -29,21 +32,81 @@ def parsing(badSites, response):
                 lat = location["latitude"]
                 lon = location["longitude"]
         except:
-            lat = "unknown"
-            lon = "unknown"
+            # if location is unknown
+            continue
         if hostName in records.keys():
-            # if host exists, increment counter
-            records[hostName]["NumVisit"] += 1
+            pass
         else:
             # if not, create entry and set visits to 1
             records[hostName] = {}
-            records[hostName]["NumVisit"] = 1
+            records[hostName]["svgPath"] = "targetSVG"
+            records[hostName]["zoomLevel"] = "5"
+            records[hostName]["scale"] = ".5"
+            records[hostName]["title"] = hostName
             records[hostName]["Latitude"] = lat
             records[hostName]["Longitude"] = lon
-            records[hostName]["IP"] = ip
+            records[hostName]["IP"] = []
             records[hostName]["KnownBad"] = 0
+            records[hostName]["NumPackets"] = 0
+            records[hostName]["TotalSize"] = 0
             if hostName in badSites:
                 records[hostName]["KnownBad"]  = 1
+    return records
+
+def getServers(records):
+    cmd = 'tshark -r web1.pcap -T fields -e dns.qry.name -e dns.a -Y "dns.flags == 0x8180"'
+    dnsRes = os.popen(cmd).read()
+    dnsRes = dnsRes.splitlines()
+    for line in dnsRes:
+        line = line.split()
+        strs = line[1]
+        strs = strs.split(",")
+        ips = []
+        for i in strs:
+            ips.append(i)
+        hostName = line[0].strip()
+
+        try:
+            records[hostName]["IP"] = ips
+        except:
+            continue
+    return records
+
+
+def packetInfo(records):
+    """
+    Get the number of packets and total size for each host.
+    :return: Dictionary with all results
+    TODO Change title field to be proper
+    """
+    cmd = "tshark -r web1.pcap -T fields -e ip.src -e ip.dst -e frame.len"
+    traffic = os.popen(cmd).read()
+    traffic = traffic.splitlines()
+    # make scope a list of all IPs in records
+    scope = []
+    # map ip to hostname for later use
+    mapping = {}
+    for host in records.keys():
+        ips = records[host]["IP"]
+        for ip in ips:
+            scope.append(ip)
+            mapping[ip] = host
+
+    for line in traffic:
+        line = line.strip().split()
+        # some packets did not have source, dest, and size. Ignore these.
+        if len(line) != 3:
+            continue
+        if line[0] == "151.101.20.193":
+            print("here")
+        if line[0] in scope:
+            records[mapping[line[0]]]["NumPackets"] += 1
+            records[mapping[line[0]]]["TotalSize"] += int(line[2])
+        elif line[1].strip() in scope:
+            records[mapping[line[1]]]["NumPackets"] += 1
+            records[mapping[line[1]]]["TotalSize"] += int(line[2])
+        else:
+            continue
     return records
 
 def getIP(hostName):
@@ -74,8 +137,10 @@ if __name__ == '__main__':
     # High has less flase positives
     level = input("Choose level (low,medium,high): ")
     badSites = knownBad(level)
-    response = os.popen('tshark -r web.pcap -T fields -e dns.qry.name -Y "dns.flags.response eq 0" | grep .com').read()
+    response = os.popen('tshark -r web1.pcap -T fields -e dns.qry.name -Y "dns.flags.response eq 0" | grep .com').read()
     response = response.splitlines()
-    results = parsing(badSites,response)
+    records = parsing(badSites,response)
+    records = getServers(records)
+    info = packetInfo(records)
     with open('result.json', 'w') as fp:
-            json.dump(results, fp)
+            json.dump(info, fp)
